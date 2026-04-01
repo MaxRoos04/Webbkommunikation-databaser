fa304b7e2e7eb7f06c1654ac5defc39aa8d2e4b9
Max Roos <71812384+MaxRoos04@users.noreply.github.com>
2026-04-01 12:39:00 +0300

    uppdate

---
+++ hotel-frontend/script.js
@@ -0,0 +1,15 @@
+ //const apiUrl= "https://roosmaxj-rahti-projekt-roosmaxj-projekt-webbkommunikation.2.rahtiapp.fi/api/ip";
+        const apiUrl= "http://127.0.0.1:8080"; 
+        async function getRooms() {
+            const res = await fetch(`${apiUrl}/rooms`);
+            const rooms = await res.json(); 
+
+            console.log(rooms)
+
+              for (room of rooms) {
+                     document.getElementById("room-list").innerHTML += `
+                     <li>${room.room_number} - ${room.room_type} - ${room.price} </li>
+                     `; 
+              }
+        }
+        getRooms()
